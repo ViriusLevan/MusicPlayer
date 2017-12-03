928a226456f462ae614bf702a541589dcc4012a4
Zephrys <alvin.valor@gmail.com>
2017-12-03 19:50:51 +0700

    Close all notification
    close all notif, but "Unfortunately, MusicPlayer has stopped. :(

--- app/src/main/java/com/example/android/musicplayer/MusicService.java
+++ app/src/main/java/com/example/android/musicplayer/MusicService.java
@@ -505,7 +505,7 @@ public class MusicService extends Service implements
     private void removeNotification() {
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.cancel(NOTIFICATION_ID);
+        notificationManager.cancelAll();
     }
 
     private PendingIntent playbackAction(int actionNumber) {
@@ -769,7 +769,7 @@ public class MusicService extends Service implements
         if (phoneStateListener != null) {
             telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_NONE);
         }
-
+        stopForeground(true);
         removeNotification();
 
         //unregister BroadcastReceivers
@@ -779,7 +779,7 @@ public class MusicService extends Service implements
         //clear cached playlist
         new StorageUtil(getApplicationContext()).clearCachedAudioPlaylist();
 
-        //stopForeground(true);
+
     }
 
     @Override
